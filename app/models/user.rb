a253c5cf2d16e91a6d33e4fe325e874531fa80c4
Bed prakash <bedprakash.kushwaha@w3villa.com>
2020-06-05 20:53:04 +0530

    grand videos access

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ActiveRecord::Base
   belongs_to :role
+  after_create :updateRoleId
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  def updateRoleId
+    binding.pry
+    userRoleId=Role.find_by(name:"user")
+    self.role_id=userRoleId.id
+    self.save
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   
   devise_for :users
+  get 'admin/updateGrantVideoAccess'
+  get 'admin/grantVideoAccess'
   get 'videos/showSpecific'
- get 'videos/showAll'
+  get 'videos/showAll'
+  
   root 'login#index'
   resources :videos
   resources :admin
+ 
 
   #root 'login#index'
   # The priority is based upon order of creation: first created -> highest priority.
